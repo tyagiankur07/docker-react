5b46b8186e3b23d47cc30203bf4acfee4e63c164
atyagi <ankur.tyagi@delphix.com>
2021-02-03 10:19:23 +0530

    xpose port 80

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There - First Docker Dev Env
+          Hi There - First Docker Dev Env.
           A new change made finally
         </p>
         <a
